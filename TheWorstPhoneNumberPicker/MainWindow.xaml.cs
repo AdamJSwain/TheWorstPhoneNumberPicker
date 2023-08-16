d2dbfe61e438d4693d4f0164ca3ceb571a191360
AdamJSwain <110462124+AdamJSwain@users.noreply.github.com>
2023-08-16 13:18:26 +0200

    Advanced mode can now be disabled.

--- TheWorstPhoneNumberPicker/MainWindow.xaml.cs
+++ TheWorstPhoneNumberPicker/MainWindow.xaml.cs
@@ -25,6 +25,8 @@ namespace TheWorstPhoneNumberPicker
         public MainWindow()
         {
             InitializeComponent();
+            sldNumber.Value = 0;
+            lblNumber.Content = 0;
             
         }
         
